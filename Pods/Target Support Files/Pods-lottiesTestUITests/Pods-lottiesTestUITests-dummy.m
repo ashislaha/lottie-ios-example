1da887aeeebb44ea8dc23906435a2e59a85e11f8
Ashis Laha <ashislaha@ashiss-mac-pro.guest.corp.microsoft.com>
2019-09-25 14:01:17 +0530

    first commit

---
+++ Pods/Target Support Files/Pods-lottiesTestUITests/Pods-lottiesTestUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_lottiesTestUITests : NSObject
+@end
+@implementation PodsDummy_Pods_lottiesTestUITests
+@end
---
+++ lottiesTest/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  lottiesTest
+//
+//  Created by Ashis Laha on 19/09/19.
+//  Copyright © 2019 Ashis Laha. All rights reserved.
+//
+
+import UIKit
+import Lottie
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        let animationView = AnimationView(name: "delivery")
+        animationView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.width * 1.1)
+        animationView.contentMode = .scaleAspectFit
+        view.addSubview(animationView)
+        
+        animationView.play()
+        animationView.loopMode = .loop
+    }
+
+
+}
+
